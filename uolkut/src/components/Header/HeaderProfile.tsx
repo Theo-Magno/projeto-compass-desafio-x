ed438274b6b123a2afcc65ca393266c0357f0430
PauloDaude <paulodaude14@gmail.com>
2023-09-23 14:36:53 -0300

    feat: done media query of header profile

--- uolkut/src/components/Header/HeaderProfile.tsx
+++ uolkut/src/components/Header/HeaderProfile.tsx
@@ -7,28 +7,34 @@ import ProfileImg from '../../assets/images/profile-img.png';
 const HeaderProfile = (): JSX.Element => {
   return (
     <header id="header-profile">
-      <h1 className="title-header-profile">UOLkut</h1>
-      <nav className="navbar">
-        {/* <Link to="#">Início</Link>
+      <div className="header-left">
+        <h1 className="title-header-profile">UOLkut</h1>
+        <nav className="navbar">
+          {/* <Link to="#">Início</Link>
         <Link to="#">Perfil</Link>
         <Link to="#">Comunidades</Link>
         <Link to="#">Jogos</Link> */}
-        <a href="#">Início</a>
-        <a href="#">Perfil</a>
-        <a href="#">Comunidades</a>
-        <a href="#">Jogos</a>
-      </nav>
-      <div className="search">
-        <input
-          type="text"
-          className="search input"
-          placeholder="Pesquisar no UOLkut"
-        />
+          <a href="#">Início</a>
+          <a href="#" className="active">
+            Perfil
+          </a>
+          <a href="#">Comunidades</a>
+          <a href="#">Jogos</a>
+        </nav>
       </div>
-      <div className="profile-header">
-        <img src={ProfileImg} alt="Foto de perfil" className="profile-img" />
-        <p>Linus Torvalds</p>
-        <Expansive />
+      <div className="header-right">
+        <div className="search">
+          <input
+            type="text"
+            className="search input"
+            placeholder="Pesquisar no UOLkut"
+          />
+        </div>
+        <div className="profile-header">
+          <img src={ProfileImg} alt="Foto de perfil" className="profile-img" />
+          <p>Linus Torvalds</p>
+          <Expansive />
+        </div>
       </div>
     </header>
   );
